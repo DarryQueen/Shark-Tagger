102ab698908d4a537bf07cd1150d7e0e60affcac
DarryQueen <slimfrog@gmail.com>
2016-03-07 00:58:19 +0000

    Add skeleton methods to UserPreference.

--- src/sharktagger/model/UserPreference.java
+++ src/sharktagger/model/UserPreference.java
@@ -1,4 +1,35 @@
 package sharktagger.model;
 
+import java.util.Set;
+
 public class UserPreference {
+    private Set<Shark> favorites;
+
+    /**
+     * If shark is not in favorites, add it to favorites.
+     * Else, remove it from favorites.
+     * @param shark Shark object to toggle.
+     */
+    public void toggleFavorite(Shark shark) {
+    }
+
+    /**
+     * Load user preferences from specified file.
+     * If file does not exist, instantiate a new preferences.
+     * @param filename String name of the preferences file.
+     * @return UserPreference object.
+     */
+    public static UserPreference retrieveFromFile(String filename) {
+        // Default return value.
+        return null;
+    }
+
+    /**
+     * Save these user preferences to a file.
+     * @return Success.
+     */
+    public boolean saveToFile() {
+        // Default return value.
+        return false;
+    }
 }
