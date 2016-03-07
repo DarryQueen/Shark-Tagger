668583eaf4db81669cf5fdf0d6de49c84796b119
DarryQueen <slimfrog@gmail.com>
2016-03-07 00:44:37 +0000

    Controller methods.

--- src/sharktagger/controller/FavoritesController.java
+++ src/sharktagger/controller/FavoritesController.java
@@ -1,4 +1,29 @@
 package sharktagger.controller;
 
-public class FavoritesController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import sharktagger.model.UserPreference;
+
+public class FavoritesController implements ActionListener {
+    /** SearchController instance to keep when opening up favorite shark. */
+    private SearchController mSearchController;
+
+    /**
+     * Standard constructor.
+     * @param pref UserPreference object.
+     * @param sController SearchController object.
+     */
+    public FavoritesController(UserPreference pref, SearchController sController) {
+    }
+
+    /**
+     * Open the favorites frame.
+     */
+    public void open() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
 }
--- src/sharktagger/controller/MenuController.java
+++ src/sharktagger/controller/MenuController.java
@@ -1,4 +1,26 @@
 package sharktagger.controller;
 
-public class MenuController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import sharktagger.model.UserPreference;
+
+public class MenuController implements ActionListener {
+    /** SearchController instance for opening up search. */
+    private SearchController mSearchController;
+    /** FavoritesController instance for opening up favorites */
+    private FavoritesController mFavoritesController;
+
+    /**
+     * Standard constructor.
+     * @param pref UserPreference object.
+     * @param sController SearchController object.
+     * @param fController FavoritesController object.
+     */
+    public MenuController(UserPreference pref, SearchController sController, FavoritesController fController) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
 }
