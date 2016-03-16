139cb8ff1dfaef55195159ebd71fbff5421b118a
DarryQueen <slimfrog@gmail.com>
2016-03-16 18:52:46 +0000

    Basic UI setup for search frame.

--- src/sharktagger/view/SearchFrame.java
+++ src/sharktagger/view/SearchFrame.java
@@ -1,6 +1,92 @@
 package sharktagger.view;
 
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextPane;
 
 public class SearchFrame extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    /** String constants. */
+    public static final String TITLE = "Search";
+
+    /** Dropdown constants. */
+    private static final String OPTION_ALL = "All";
+
+    private static final String RANGE_DAY = "Last 24 Hours";
+    private static final String RANGE_WEEK = "Last Week";
+    private static final String RANGE_MONTH = "Last Month";
+    private static final String[] RANGE_OPTIONS = {RANGE_DAY, RANGE_WEEK, RANGE_MONTH};
+
+    private static final String GENDER_MALE = "Male";
+    private static final String GENDER_FEMALE = "Female";
+    private static final String[] GENDER_OPTIONS = {OPTION_ALL, GENDER_MALE, GENDER_FEMALE};
+
+    private static final String STAGE_MATURE = "Mature";
+    private static final String STAGE_IMMATURE = "Immature";
+    private static final String STAGE_UNDETERMINED = "Undetermined";
+    private static final String[] STAGE_OPTIONS = {OPTION_ALL, STAGE_MATURE, STAGE_IMMATURE, STAGE_UNDETERMINED};
+
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 1000;
+    private static final int DIVIDER_LOCATION = 300;
+
+    /** Instance variables. */
+    private ActionListener mActionListener;
+
+    private JComboBox<String> jcbRange;
+    private JComboBox<String> jcbGender;
+    private JComboBox<String> jcbStage;
+    private JComboBox<String> jcbLocation;
+
+    private JTextPane jtpResults;
+
+    private void setupUI() {
+        // Left side query.
+        JPanel queryPanel = new JPanel();
+        queryPanel.add(jcbRange);
+        queryPanel.add(jcbGender);
+        queryPanel.add(jcbStage);
+        queryPanel.add(jcbLocation);
+
+        // Right side search results.
+        JPanel resultsPanel = new JPanel();
+        resultsPanel.add(jtpResults);
+        JScrollPane resultsScrollPane = new JScrollPane(resultsPanel);
+
+        // TODO: Setup the JTextPane.
+
+        // JSplitPane.
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        splitPane.setDividerLocation(DIVIDER_LOCATION);
+        splitPane.setLeftComponent(queryPanel);
+        splitPane.setRightComponent(resultsScrollPane);
+
+        this.add(splitPane);
+    }
+
+    public SearchFrame(ActionListener listener, List<String> locations) {
+        super(TITLE);
+        this.setSize(WIDTH, HEIGHT);
+
+        locations.add(0, OPTION_ALL);
+        String[] locationsArray = new String[locations.size()];
+
+        mActionListener = listener;
+
+        jcbRange = new JComboBox<String>(RANGE_OPTIONS);
+        jcbGender = new JComboBox<String>(GENDER_OPTIONS);
+        jcbStage = new JComboBox<String>(STAGE_OPTIONS);
+        jcbLocation = new JComboBox<String>(locations.toArray(locationsArray));
+
+        jtpResults = new JTextPane();
+
+        setupUI();
+    }
 }
