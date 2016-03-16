9bcf6b948bc901fd83c9d405bf3cb8b49e9eab1a
DarryQueen <slimfrog@gmail.com>
2016-03-16 19:54:58 +0000

    Make static fields public.

--- src/sharktagger/view/SearchFrame.java
+++ src/sharktagger/view/SearchFrame.java
@@ -22,20 +22,20 @@ public class SearchFrame extends JFrame {
     public static final String JBSEARCH_NAME = "sharktagger.view.SearchFrame.jbSearch";
 
     /** Dropdown constants. */
-    private static final String OPTION_ALL = "All";
+    public static final String OPTION_ALL = "All";
 
-    private static final String RANGE_DAY = "Last 24 Hours";
-    private static final String RANGE_WEEK = "Last Week";
-    private static final String RANGE_MONTH = "Last Month";
+    public static final String RANGE_DAY = "Last 24 Hours";
+    public static final String RANGE_WEEK = "Last Week";
+    public static final String RANGE_MONTH = "Last Month";
     private static final String[] RANGE_OPTIONS = {RANGE_DAY, RANGE_WEEK, RANGE_MONTH};
 
-    private static final String GENDER_MALE = "Male";
-    private static final String GENDER_FEMALE = "Female";
+    public static final String GENDER_MALE = "Male";
+    public static final String GENDER_FEMALE = "Female";
     private static final String[] GENDER_OPTIONS = {OPTION_ALL, GENDER_MALE, GENDER_FEMALE};
 
-    private static final String STAGE_MATURE = "Mature";
-    private static final String STAGE_IMMATURE = "Immature";
-    private static final String STAGE_UNDETERMINED = "Undetermined";
+    public static final String STAGE_MATURE = "Mature";
+    public static final String STAGE_IMMATURE = "Immature";
+    public static final String STAGE_UNDETERMINED = "Undetermined";
     private static final String[] STAGE_OPTIONS = {OPTION_ALL, STAGE_MATURE, STAGE_IMMATURE, STAGE_UNDETERMINED};
 
     private static final int WIDTH = 1000;
