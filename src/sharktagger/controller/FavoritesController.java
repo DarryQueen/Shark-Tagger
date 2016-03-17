3471a565d721733a2df8e586e19fa04b0da83422
DarryQueen <slimfrog@gmail.com>
2016-03-17 22:05:13 +0000

    Add distance calculations to favorites.

--- src/sharktagger/controller/FavoritesController.java
+++ src/sharktagger/controller/FavoritesController.java
@@ -6,16 +6,21 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.swing.JButton;
 
 import api.jaws.Jaws;
+import api.jaws.Location;
 import api.jaws.Shark;
 import sharktagger.model.UserPreference;
 import sharktagger.view.FavoritesFrame;
 
 public class FavoritesController implements ActionListener, FocusListener {
+    public static final Location KINGS_LOCATION = new Location(51.5119, 0.1161);
+
     /** SearchController instance to keep when opening up favorite shark. */
     private SearchController mSearchController;
 
@@ -51,6 +56,11 @@ public class FavoritesController implements ActionListener, FocusListener {
         mFavoritesFrame.setVisible(true);
     }
 
+    private double getDistanceFromKings(Shark shark) {
+        Location location = mJaws.getLastLocation(shark.getName());
+        return calcDistance(KINGS_LOCATION, location);
+    }
+
     private void refreshFavorites() {
         mFavoritesFrame.clearFavorites();
 
@@ -62,10 +72,20 @@ public class FavoritesController implements ActionListener, FocusListener {
             sharks.add(shark);
         }
 
-        // TODO: Sort the list by distance from King's.
+        // Sort the list by distance from King's.
+        Collections.sort(sharks, new Comparator<Shark>() {
+            @Override
+            public int compare(Shark s1, Shark s2) {
+                double d1 = getDistanceFromKings(s1);
+                double d2 = getDistanceFromKings(s2);
+
+                return Double.compare(d1, d2);
+            }
+        });
 
         for (Shark shark : sharks) {
-            mFavoritesFrame.addFavorite(shark);
+            double distance = getDistanceFromKings(shark);
+            mFavoritesFrame.addFavorite(shark, distance);
         }
     }
 
@@ -94,5 +114,25 @@ public class FavoritesController implements ActionListener, FocusListener {
     @Override
     public void focusLost(FocusEvent e) {
         // Do nothing.
+    }
+
+    /**
+     * Grab distance between two Location objects in kilometers.
+     * @param l1 Location object.
+     * @param l2 Location object.
+     * @return Double distance in kilometers.
+     */
+    public static double calcDistance(Location l1, Location l2) {
+        double lat1 = l1.getLatitude(), lng1 = l1.getLongitude();
+        double lat2 = l2.getLatitude(), lng2 = l2.getLongitude();
+
+        double earthRadius = 6371;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double dist = earthRadius  * c;
+
+        return dist;
     }
 }
